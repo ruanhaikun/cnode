d9481d9a4d45cf9142556f7a403ffbcc0b47cc18
ruanhaikun <ruanhaikun@qq.com>
2019-08-12 09:59:38 +0800

    test

--- src/view/main-footer.js
+++ src/view/main-footer.js
@@ -5,7 +5,7 @@ import { Layout } from 'antd';
 class MainFooter extends Component {
     render() {
         return <Layout.Footer style={{ textAlign: 'center' }}>
-            京ICP备07102442号-1，2001-2002 版权所有
+            京ICP备07102442号-1，2001-2002 版权所有a
         </Layout.Footer>
     }
 }
